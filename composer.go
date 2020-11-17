f42efc598d50f83aca47e61a1848a3da63478c8c
Gabe Cook <gabe@clevyr.com>
2020-11-17 15:40:45 -0600

    :zap: Do not attempt to install a Composer dependency if it is already installed

--- composer.go
+++ composer.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"github.com/clevyr/scaffold/appconfig"
 	"github.com/clevyr/scaffold/iexec"
+	"os/exec"
 )
 
 func composerRequire(appConfig appconfig.AppConfig) (err error) {
-	param := []string{"require"}
-	devParam := []string{"require", "--dev"}
+	var param, devParam []string
 
 	for name, module := range appConfig.ComposerDeps {
-		if module.Enabled {
+		if module.Enabled && !composerInstalled(name) {
 			var appParam string
 
 			if module.Version == "" {
@@ -29,12 +29,18 @@ func composerRequire(appConfig appconfig.AppConfig) (err error) {
 	}
 
 	if len(devParam) > 0 {
-		err = iexec.Command("composer", devParam...)
+		err = iexec.Command("composer", append([]string{"require", "--dev"}, devParam...)...)
 	}
 
 	if len(param) > 0 {
-		err = iexec.Command("composer", param...)
+		err = iexec.Command("composer", append([]string{"require"}, param...)...)
 	}
 
 	return
+}
+
+func composerInstalled(name string) bool {
+	cmd := exec.Command("composer", "show", name)
+	err := cmd.Run()
+	return err == nil
 }
